46cb5bf867c39cbe6521907400a1b28c7bdfa248
Y.S.I.C.Kumaranayake <153353978+Chathuminays@users.noreply.github.com>
2024-07-27 08:01:54 +0530

    sidebar added.

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,38 @@
+// import { useSidebar } from '../context/SidebarContext';
+import { IoIosClose } from "react-icons/io";
+
+// Define the types for the SidebarContext
+interface SidebarContextType {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}
+
+const SideBar: React.FC = () => {
+  // Using custom hook from SidebarContext to manage sidebar state
+//   const { isOpen, toggleSidebar } = useSidebar() as SidebarContextType;
+
+  // Function to close the sidebar
+//   const handleClose = () => {
+//     toggleSidebar();
+//   };
+
+  return (
+    <div className={`xs:w-64 md:w-64 xl:w-56 2xl:w-64 z-30 bg-purple fixed h-full xs:mt-0 lg:mt-0 transition-transform duration-300 lg:translate-x-0`}>
+      {/* Close button for mobile view */}
+      <div className='w-6 h-6 rounded-lg bg-purple absolute top-2 right-2 flex items-center justify-center lg:hidden'>
+        <IoIosClose className='w-6 h-6 text-white cursor-pointer'/>
+      </div>
+
+      <div className='h-20 bg-purple_light flex items-center justify-center'>
+        <h3 className='text-white font-semibold xs:text-xl lg:text-2xl'>Acmy Solutions</h3>
+      </div>
+
+      <div className='flex items-center xs:w-56 xl:w-52 2xl:w-56 h-10 py-3 px-4 mx-auto mt-10 gap-4 bg-purple_light rounded-full cursor-pointer'>
+        <img src='/assets/icon.svg' alt="Dashboard Icon" />
+        <p className='text-white'>Dashboard</p>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,29 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'xs': '320px',
+      'sm': '425px',
+      'md': '768px',
+      'lg': '800px',
+      'xl': '1024px',
+      '2xl': '1280px',
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        purple: 'hsla(277, 84%, 17%, 1)',
+        purple_light: 'hsla(283, 16%, 92%, 0.1)',
+        background: 'hsla(0, 0%, 98%, 1)',
+        stroke: 'hsla(217, 16%, 84%, 1)',
+        text_grey: 'hsla(0, 0%, 46%, 1)',
+        text_pink: 'hsla(325, 100%, 37%, 1)',
+        bg_grey: 'hsla(0, 0%, 95%, 1)',
+        text_yellow: 'hsla(45, 86%, 62%, 1)',
+        text_blue: 'hsla(214, 84%, 56%, 1)',
+        text_red: 'hsla(0, 79%, 63%, 1)',
+        bg_yellow: 'hsla(45, 86%, 62%, 0.1)',
+        bg_blue: 'hsla(214, 84%, 56%, 0.1)',
+        bg_red: 'hsla(0, 79%, 63%, 0.1)',
       },
     },
   },
